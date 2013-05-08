ca1679c379820e6fd6ef40eb8dd76d2b7747cf0a
vpandin <viniciuspandin@gmai.com>
2013-05-08 15:41:34 -0300

    mais testes

--- TestGit/src/teste/GitTeste.java
+++ TestGit/src/teste/GitTeste.java
@@ -5,6 +5,14 @@ public class GitTeste {
 public static void main(String[] args) {
 	
 	System.out.println("outra calsse!!");
+	
+	int a=0;
+	int b =5;
+	int c = 10;
+	
+	a = b * c;
+	
+	System.out.printiln(a);
 }
 
 }
